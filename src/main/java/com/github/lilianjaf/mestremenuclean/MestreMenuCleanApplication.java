d95a24771810b73ed4e7094f960359e528323385
Lilian Ferreira <lilianjaf@gmail.com>
2026-03-19 22:31:42 -0300

    create main application entry point

---
+++ src/main/java/com/github/lilianjaf/mestremenuclean/MestreMenuCleanApplication.java
@@ -0,0 +1,13 @@
+package com.github.lilianjaf.mestremenuclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MestreMenuCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MestreMenuCleanApplication.class, args);
+	}
+
+}
